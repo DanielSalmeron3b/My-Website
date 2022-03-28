c9de6e28008e1d88c89b69e3ccf354622318c716
DanielSalmeron3b <danielesajr@gmail.com>
2022-03-28 10:13:19 -0600

    🔃 Added the random background functionality

--- JS/main.js
+++ JS/main.js
@@ -1,17 +1,23 @@
 const hamburguerButton = document.getElementById("hamburguer-menu"),
     menu = document.getElementById("menu");
 
+let randomBackgrounds = [];
+
+randomBackgrounds[0] = '../assets/img/pexels-daniel-perez-sea.png';
+randomBackgrounds[1] = '../assets/img/conchalio-beach-1.png';
+randomBackgrounds[2] = '../assets/img/el-palmarcito-beach.png';
+randomBackgrounds[3] = '../assets/img/conchalio-beach-2.png';
+randomBackgrounds[4] = '../assets/img/soyapango-sky.png';
+
+window.onload = ()=> {
+    let setRandomBackground = Math.floor(Math.random() * randomBackgrounds.length);
+    document.body.style.backgroundImage = `url(${randomBackgrounds[setRandomBackground]})`;
+}
+
 hamburguerButton.addEventListener('click', ()=> {
 
     // Alternate styles for the menu
     menu.classList.toggle("active-menu");
     document.body.classList.toggle("opacity-menu");
 
-    // const routeMenu = e.target.getAttribute('src');
-
-    // if(routeMenu == 'assets/img/menu-icon.png'){
-    //     e.target.setAttribute('src', 'assets/img/menu-icon.png')
-    // }else{
-    //     e.target.setAttribute('src', 'assets/img/menu-icon.png')
-    // }
 });
