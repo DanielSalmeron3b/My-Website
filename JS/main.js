ea148846eb0cf7cd321d25ef8e7a28a400c9278f
DanielSalmeron3b <danielesajr@gmail.com>
2022-04-20 14:01:02 -0600

    Started to work on the Projects page

--- JS/main.js
+++ JS/main.js
@@ -3,11 +3,10 @@ const hamburguerButton = document.getElementById("hamburguer-menu"),
 
 let randomBackgrounds = [];
 
-randomBackgrounds[0] = '../assets/img/pexels-daniel-perez-sea.png';
-randomBackgrounds[1] = '../assets/img/conchalio-beach-1.png';
-randomBackgrounds[2] = '../assets/img/el-palmarcito-beach.png';
-randomBackgrounds[3] = '../assets/img/conchalio-beach-2.png';
-randomBackgrounds[4] = '../assets/img/soyapango-sky.png';
+randomBackgrounds[0] = '../assets/backgrounds/bkg1.jpg';
+randomBackgrounds[1] = '../assets/backgrounds/bkg2.jpg';
+randomBackgrounds[2] = '../assets/backgrounds/bkg3.jpg';
+randomBackgrounds[3] = '../assets/backgrounds/bkg4.jpg';
 
 window.onload = ()=> {
     let setRandomBackground = Math.floor(Math.random() * randomBackgrounds.length);
