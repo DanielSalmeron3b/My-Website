3be94859f4e6521794494e1342301669f3161790
DanielSalmeron3b <danielesajr@gmail.com>
2022-05-04 13:43:11 -0600

    🦶 Added the footer section

--- layouts/footer.php
+++ layouts/footer.php
@@ -1,4 +1,60 @@
-<footer>
+<footer id="site-footer">
+
+    <section class="horizontal-footer-section" id="footer-top-section">
+        <div id="footer-top-menu-container" role="menubar">
+            <ul id="footer-top-menu" role="menu">
+                <li class="footer-top-menu-item" role="menuitem">
+                    <a href="#" class="footer-top-menu-link">IT Services</a>
+                </li>
+                <li class="footer-top-menu-item footer-top-menu-item-active" role="menuitem">
+                    <a href="#" class="footer-top-menu-link">Web development</a>
+                </li>
+                <li class="footer-top-menu-item" role="menuitem">
+                    <a href="#" class="footer-top-menu-link">SEO</a>
+                </li>
+                <li class="footer-top-menu-item" role="menuitem">
+                    <a href="#" class="footer-top-menu-link">UI/UX</a>
+                </li>
+            </ul>
+        </div>
+        <div id="footer-buttons-container">
+            <a href="#" class="footer-button" role="button">View our Projects</a>
+        </div>
+    </section>
+
+    <!-- <section class="horizontal-footer-section" id="footer-middle-section">
+        <div id="footer-about" class="footer-columns footer-columns-large">
+        </div>
+        <div class="footer-columns">
+            <h1>Overview</h1>
+            <ul class="footer-column-menu" role="menu">
+                <li class="footer-column-menu-item" role="menuitem">
+                    <a href="#" class="footer-column-menu-item-link">Services </a>
+                </li>
+                <li class="footer-column-menu-item" role="menuitem">
+                    <a href="#" class="footer-column-menu-item-link">Pricing</a>
+                </li>
+                <li class="footer-column-menu-item" role="menuitem">
+                    <a href="#" class="footer-column-menu-item-link">Portfolio</a>
+                </li>
+                <li class="footer-column-menu-item" role="menuitem">
+                    <a href="#" class="footer-column-menu-item-link">News</a>
+                </li>
+            </ul>
+        </div>
+    </section> -->
+    
+    <section class="horizontal-footer-section" id="footer-bottom-section">
+        <div id="footer-copyright-info">
+            &copy; Daniel Salmerón. 2022.
+        </div>
+        <div id="footer-social-buttons">
+            <img src="https://img.icons8.com/material-outlined/25/000000/github.png"/>
+            <img src="https://img.icons8.com/ios-glyphs/25/000000/instagram-new.png"/>
+            <img src="https://img.icons8.com/ios-glyphs/25/000000/linkedin-circled--v1.png"/>
+            <img src="https://img.icons8.com/ios-filled/25/000000/behance.png"/>
+        </div>
+    </section>
 
 </footer>
 
